5eaabee97e9b6290b3f8050d244fd15de7c4b10e
manaslaud <manaslaud@hotmail.com>
2024-03-07 21:54:48 +0530

    feat:removed horizontal scrollbar

--- app/sections/Organisers.tsx
+++ app/sections/Organisers.tsx
@@ -3,7 +3,7 @@ export default function Organisers(){
 return (
     <main className="flex flex-col justify-center items-start gap-[1.5rem] px-[3rem]">
         <h1 className="text-[#eaeaea] text-6xl font-ptMono">ORGANISERS</h1>
-<section className="flex justify-around items-center w-full overflow-x-auto whitespace-nowrap ">
+<section className="flex justify-around items-center w-full overflow-x-auto whitespace-nowrap no-scrollbar::-webkit-scrollbar no-scrollbar">
         <div className="flex flex-col justify-center items-center flex-shrink-0 gap-[1rem] ">
         <Image width={200} height={200} alt="Profile Photo" src={'/yantra/img.svg'} className=" w-[50%] rounded-full"/>
         <p className=" font-ptMono  text-[#eaeaea] text-3xl ">G. Viswanathan</p>
--- app/sections/Patrons.tsx
+++ app/sections/Patrons.tsx
@@ -1,9 +1,9 @@
 import PatronsCard from "../components/PatronsCard"
 export default function Patrons(){
     return (
-      <main className="w-full flex flex-col justify-between px-[3rem] items-start gap-[2rem]">
+      <main className="w-full flex flex-col justify-between px-[3rem] items-start gap-[2rem] ">
                 <h1 className="text-[#eaeaea] text-6xl font-ptMono">PATRONS</h1>
-          <section className="w-full grid grid-cols-4 px-[3rem] gap-[2rem]">
+          <section className="w-full grid grid-cols-4 px-[3rem] gap-[2rem] ">
           <PatronsCard imgUrl={null} name={null} designation={null} />
           <PatronsCard imgUrl={null} name={null} designation={null} />
             <PatronsCard imgUrl={null} name={null} designation={null} />
