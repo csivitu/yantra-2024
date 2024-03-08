118ef34fce8466b3bddf93883ee3179b64c88027
manaslaud <manaslaud@hotmail.com>
2024-03-09 01:53:41 +0530

    FIX:PLACEHOLDER CHANGE

--- app/sections/Yantra.tsx
+++ app/sections/Yantra.tsx
@@ -16,7 +16,7 @@ export default function Yantra(){
             </section>
             <section className="flex flex-col laptop:w-[52%] gap-[12px] laptop:h-full mobile:w-full mobile:gap-[1rem]">
                 <div className=" laptop:text-[375px] laptop:h-[360px] text-transparent bg-clip-text border-[1px] border-[#292929] rounded-[20px]    overflow-x-clip bg-gradient-radial from-[#B6C2FF] to-[#00051D] font-schabo  whitespace-nowrap  justify-center items-center leading-none flex mobile:text-[100px] mobile:text-center">
-               <p className="w-full laptop:mt-[90px] mobile:mt-[25px]"> ANNUAL TECH</p>
+               <p className="w-full laptop:mt-[90px] mobile:mt-[25px]"> ANNUAL</p>
                 </div>
                 <div className="font-ptMono text-white text-2xl border-[1px] border-[#292929] rounded-[20px] px-[28px] py-[28px] leading-9 ">
                 Riviera is the Annual International Sports and Cultural Carnival of the Vellore Institute of Technology. It is an International 4-day event that consists of sports competitions, social and cultural events along with concerts. The 2016 edition of Riviera witnessed a footfall of more than 30,000 students from over 650 colleges from across the country. It is organised primarily by the students of the college. It has a wide variety of competitions and events, which range from dramatic, literary, musical, dance, debate 
