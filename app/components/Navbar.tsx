4e577923cf87384718d4dba5435ddcf31289d07f
manaslaud <manaslaud@hotmail.com>
2024-03-09 12:08:58 +0530

    fix:capitalization

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,11 +4,11 @@ export default function Navbar(){
         <nav className="flex w-full bg-black z-[2] items-center justify-between rounded-[12px] border-[1px] border-[#fff] border-opacity-[18%] px-[22px] py-[12px] text-[1rem]">
             <Image src='/hero/vitLogo.svg' alt="Logo" width={130} height={130} className="mobile:w-[40%] laptop:w-[9%]"/>
             <div className=" laptop:w-[45%]  justify-around items-center text-white font-ptMono font-light laptop:flex mobile:hidden">
-                <p>ABOUT US</p>
-                <p>EVENTS</p>
-                <p>TEAM</p>
-                <p>SPONSORS</p>
-                <p>CONTACT US</p>
+                <p>About us</p>
+                <p>Events</p>
+                <p>Team</p>
+                <p>Sponsors</p>
+                <p>Contact us</p>
             </div>
         </nav>
     )
