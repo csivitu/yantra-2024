57618baf74a38de0e35d21b2a0fc2727439b5d22
manaslaud <manaslaud@hotmail.com>
2024-03-09 12:07:59 +0530

    fix:navbar padding

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 export default function Navbar(){
     return (
-        <nav className="flex w-full bg-black z-[2] items-center justify-between rounded-[12px] border-[2px] border-[#fff] border-opacity-[18%] px-[28px] py-[16px] text-lg">
-            <Image src='/hero/vitLogo.svg' alt="Logo" width={150} height={150} className="mobile:w-[40%] laptop:w-[10%]"/>
-            <div className=" w-[30%] justify-between items-center text-white font-ptMono font-light laptop:flex mobile:hidden">
+        <nav className="flex w-full bg-black z-[2] items-center justify-between rounded-[12px] border-[1px] border-[#fff] border-opacity-[18%] px-[22px] py-[12px] text-[1rem]">
+            <Image src='/hero/vitLogo.svg' alt="Logo" width={130} height={130} className="mobile:w-[40%] laptop:w-[9%]"/>
+            <div className=" laptop:w-[45%]  justify-around items-center text-white font-ptMono font-light laptop:flex mobile:hidden">
                 <p>ABOUT US</p>
                 <p>EVENTS</p>
                 <p>TEAM</p>
