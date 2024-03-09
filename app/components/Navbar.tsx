f81c86ed72c613ccaf7064228487f360e0cadb04
manaslaud <manaslaud@hotmail.com>
2024-03-09 12:12:16 +0530

    small fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 export default function Navbar(){
     return (
-        <nav className="flex w-full bg-black z-[2] items-center justify-between rounded-[12px] border-[1px] border-[#fff] border-opacity-[18%] px-[22px] py-[12px] text-[1rem]">
+        <nav className="flex w-full bg-black z-[2] items-center justify-between rounded-[12px] border-[1px] border-[#fff] border-opacity-[18%] px-[22px] py-[12px] text-[1.2rem]">
             <Image src='/hero/vitLogo.svg' alt="Logo" width={130} height={130} className="mobile:w-[40%] laptop:w-[9%]"/>
-            <div className=" laptop:w-[45%]  justify-around items-center text-white font-ptMono font-light laptop:flex mobile:hidden">
+            <div className=" laptop:w-[53%]  justify-around items-center text-white font-ptMono font-light laptop:flex mobile:hidden">
                 <p>About us</p>
                 <p>Events</p>
                 <p>Team</p>
