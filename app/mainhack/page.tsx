2332bf0055733433060e9eb2b6e7a2b6c9df9784
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-03-14 00:31:16 +0530

    Tracks Component Done

---
+++ app/mainhack/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "../sections/Footer";
+import Timeline from "../sections/Timeline";
+import Navbar from "../components/Navbar";
+import Tracks from "../sections/Tracks";
+
+const page = () => {
+  return (
+    <main className="w-full gap-[4rem] flex flex-col py-[1rem] font-schabo bg-black">
+      <Navbar />
+      <Timeline />
+      {/* Try this with gridOption = 3 and gridOption = 4. 3 looks better imo */}
+      <Tracks gridOption={4} />
+      <Footer />
+    </main>
+  );
+};
+
+export default page;
