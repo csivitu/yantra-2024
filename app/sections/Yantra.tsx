fe7797424af4e857c8325f504678ca99d109dd68
manaslaud <manaslaud@hotmail.com>
2024-03-14 14:30:32 +0530

    feaat:animation

--- app/sections/Yantra.tsx
+++ app/sections/Yantra.tsx
@@ -13,8 +13,10 @@ export default function Yantra() {
         </div>
       </section>
       <section className="flex flex-col laptop:w-[52%] gap-[12px] laptop:h-full mobile:w-full mobile:gap-[1rem]">
-        <div className=" laptop:text-[375px] laptop:h-[360px] text-transparent bg-clip-text border-[1px] border-[#292929] rounded-[20px]    overflow-x-clip bg-gradient-radial from-[#B6C2FF] to-[#00051D] font-schabo  whitespace-nowrap  justify-center items-center leading-none flex mobile:text-[100px] mobile:text-center">
-          <p className="w-full laptop:mt-[90px] mobile:mt-[25px]"> ANNUAL</p>
+        <div className=" laptop:text-[375px] laptop:h-[360px] text-transparent bg-clip-text border-[1px] border-[#292929] rounded-[20px]  bg-gradient-radial from-[#B6C2FF] to-[#00051D] font-schabo  whitespace-nowrap   justify-between items-center leading-none flex mobile:text-[100px] mobile:text-center  ">
+          <p className="w-full laptop:mt-[90px] mobile:mt-[25px] marquee px-[0.6rem]">ANNUAL</p>
+          <p className="w-full laptop:mt-[90px] mobile:mt-[25px] marquee px-[0.6rem]"> ANNUAL</p>
+          <p className="w-full laptop:mt-[90px] mobile:mt-[25px] marquee px-[0.6rem]"> ANNUAL</p>
         </div>
         <div className="font-ptMono text-white text-2xl border-[1px] border-[#292929] rounded-[20px] px-[28px] py-[28px] leading-9 ">
         Yantra, VIT's annual tech fest, celebrates innovation with hackathons, coding challenges, workshops, and talks. It promotes sustainable goals, urging students to tackle real-world issues. The fest fosters a tech community for collaboration and skill showcases, emphasizing creativity and problem-solving. Yantra is a highly anticipated event inspiring students to drive positive change.
