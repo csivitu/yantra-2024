7e467162d419c2d64272c55ed191df202d484b32
woaitsAryan <yoprince.ab@gmail.com>
2024-04-09 15:36:40 +0530

    feat: Profile page styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -63,47 +63,58 @@ export default function Dashboard() {
     };
 
     return (
-        <main className="bg-black min-h-screen text-white">
+        <main className="bg-black min-h-screen text-white flex flex-col justify-center items-center">
             {/* {sessionUser && <h1>Welcome {sessionUser.name}</h1>} */}
             <h2 className="w-full text-6xl font-ptMono text-center">Dashboard</h2>
             {/* {team && <h3>{team.teamName} and {team.teamCode}</h3>} */}
-            <h2 className="w-full text-3xl font-ptMono">Profile</h2>
-            {/* {user && <p>{user.linkedinProfile}</p>} */}
-            {/* {profileSet.valueOf() && <Link href="/test/profile">Kindly complete your profile, it's important for selection</Link>} */}
-            
-            {true && (
-                <div className="flex flex-col gap-[1rem] justify-center items-start w-full">
-                    <p className="font-bold text-xl">Name: <span className="font-normal">Manas</span></p>
-                    <p className="font-bold text-xl">Email: <span className="font-normal">manaslaud2004@gmail.com</span></p>
-                    <p className="font-bold text-xl w-[75%]">About me: <span className="font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quibusdam molestiae aliquam totam corporis, suscipit numquam aperiam quidem dolore sapiente quas vitae. Praesentium excepturi omnis minima? Labore iusto deserunt natus.</span></p>
-                    {/* user Github && */}
-                    <div className="w-full flex flex-col gap-[1rem]">
-                    <p className="font-bold text-xl w-[75%]">Github: <span className="font-normal">manaslaud</span></p>
-                    </div>
-                </div>
-            )}
-           <div className="flex flex-col gap-[1rem] justify-center items-center">
-           <h2 className="w-full text-3xl font-ptMono">Idea Submission</h2>
-           <form onSubmit={handleFormSubmit} className="w-full flex justify-center items-center gap-[2rem] ">
-                <label className="flex flex-col gap-[1rem] justify-center items-center ">
-                    Idea Description:
-                    <input type="text" name="ideaDescription" value={ideaData.ideaDescription} onChange={handleIdeaDataChange} required   className="border-[2px] border-[#fafafa] bg-black text-gray-400 font-ptMono px-[1rem] rounded-lg py-[0.5rem] text-xl"
-/>
-                </label>
-                <label className="flex flex-col gap-[1rem] justify-center items-center ">
-                    Idea Link:
-                    <input type="text" name="ideaLink" value={ideaData.ideaLink} onChange={handleIdeaDataChange} required   className="border-[2px] border-[#fafafa] bg-black text-gray-400 font-ptMono px-[1rem] rounded-lg py-[0.5rem] text-xl"
-/>
-                </label>
-                <label className="flex flex-col gap-[1rem] justify-center items-center ">
-                    Track:
-                    <input type="text" name="track" value={ideaData.track} onChange={handleIdeaDataChange} required   className="border-[2px] border-[#fafafa] bg-black text-gray-400 font-ptMono px-[1rem] rounded-lg py-[0.5rem] text-xl"
- />
-                </label>
-            </form>
-            <button type="submit" className="flex flex-row gap-4 justify-center items-center font-ptMono p-3 font-light  text-xl rounded-lg text-white bg-blue-500 border-b-4 border-transparent hover:scale-110 hover:border-b-4  hover:border-blue-700 transition-all duration-200 ease-in-out">Submit</button>
+            <section className = "flex flex-col justify-center items-center w-[80%]">
+                <h2 className="w-full text-3xl font-ptMono">Profile</h2>
+                {/* {user && <p>{user.linkedinProfile}</p>} */}
+                {/* {profileSet.valueOf() && <Link href="/test/profile">Kindly complete your profile, it's important for selection</Link>} */}
 
-           </div>
+                {user && (
+                    <div className="flex flex-col gap-[1rem] justify-center items-start w-full">
+                        <p className="font-bold text-xl">Name: <span className="font-normal">{user.name}</span></p>
+                        <p className="font-bold text-xl">Email: <span className="font-normal">{user.email}</span></p>
+                        <p className="font-bold text-xl w-[75%]">About me: <span className="font-normal">{user.bio}</span></p>
+                        {/* user Github && */}
+                        <div className="w-full flex flex-col gap-[1rem]">
+                            <p className="font-bold text-xl w-[75%]">Github: <span className="font-normal">{user.githubProfile}</span></p>
+                        </div>
+                    </div>
+                )}
+            {/* <section className = "flex flex-col ">
+                {team && (
+                    <div>
+                        {team.members.map((member, index) => () => {}
+                    </div>
+                )
+
+                }
+            </section> */}
+            </section>
+            <div className="flex flex-col gap-[1rem] justify-center items-center">
+                <h2 className="w-full text-3xl font-ptMono text-center">Idea Submission</h2>
+                <form onSubmit={handleFormSubmit} className="w-full flex justify-center items-center gap-[2rem] ">
+                    <label className="flex flex-col gap-[1rem] justify-center items-center ">
+                        Idea Description:
+                        <input type="text" name="ideaDescription" value={ideaData.ideaDescription} onChange={handleIdeaDataChange} required className="border-[2px] border-[#fafafa] bg-black text-gray-400 font-ptMono px-[1rem] rounded-lg py-[0.5rem] text-xl"
+                        />
+                    </label>
+                    <label className="flex flex-col gap-[1rem] justify-center items-center ">
+                        Idea Link:
+                        <input type="text" name="ideaLink" value={ideaData.ideaLink} onChange={handleIdeaDataChange} required className="border-[2px] border-[#fafafa] bg-black text-gray-400 font-ptMono px-[1rem] rounded-lg py-[0.5rem] text-xl"
+                        />
+                    </label>
+                    <label className="flex flex-col gap-[1rem] justify-center items-center ">
+                        Track:
+                        <input type="text" name="track" value={ideaData.track} onChange={handleIdeaDataChange} required className="border-[2px] border-[#fafafa] bg-black text-gray-400 font-ptMono px-[1rem] rounded-lg py-[0.5rem] text-xl"
+                        />
+                    </label>
+                    <button type="submit" className="flex flex-row gap-4 justify-center items-center font-ptMono p-3 font-light  text-xl rounded-lg text-white bg-blue-500 border-b-4 border-transparent hover:scale-110 hover:border-b-4  hover:border-blue-700 transition-all duration-200 ease-in-out">Submit</button>
+                </form>
+
+            </div>
         </main>
     )
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -64,38 +64,38 @@ export default function ProfilePage() {
     };
 
     return (
-        <main>
-            <h1>Profile</h1>
-            <div>
+        <main className='font-ptMono bg-black min-h-screen w-full text-white p-8'>
+            <h1 className='text-4xl font-bold'>Profile</h1>
+            <div className='border-2 border-white p-4 rounded-lg mt-4'>
                 {user && (
-                    <>
-                        <h1>{user.name}</h1>
-                        <p>{user.email}</p>
-                        <Image width={100} height={100} src={user.image} alt={user.name} />
-                    </>
+                    <div className='flex flex-col md:flex-row gap-4 items-center'>
+                        <Image width={100} height={100} src={user.image} alt={user.name} className='rounded-full'/>
+                        <div className=""><h1 className='text-xl font-medium '>{user.name}</h1>
+                        <p className='text-sm text-[#aeaeae]'>{user.email}</p></div>
+                    </div>
                 )}
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Github Profile:
-                        <input type="text" value={githubProfile} onChange={(e) => setGithubProfile(e.target.value)} />
+                <form onSubmit={handleSubmit} className='mt-4 flex flex-col gap-4'>
+                    <label className='flex w-full md:w-[60vw] flex-col md:flex-row'>
+                        <div className="w-full md:w-[30%]">Github Profile:</div>
+                        <input type="text" value={githubProfile} onChange={(e) => setGithubProfile(e.target.value)} className='rounded-lg p-1'/>
                     </label>
-                    <label>
-                        LinkedIn Profile:
-                        <input type="text" value={linkedinProfile} onChange={(e) => setLinkedinProfile(e.target.value)} />
+                    <label className='flex w-full md:w-[60vw] flex-col md:flex-row'>
+                        <div className="w-full md:w-[30%]">LinkedIn Profile:</div>
+                        <input type="text" value={linkedinProfile} onChange={(e) => setLinkedinProfile(e.target.value)} className='rounded-lg p-1'/>
                     </label>
-                    <label>
-                        Other Profile:
-                        <input type="text" value={otherProfile} onChange={(e) => setOtherProfile(e.target.value)} />
+                    <label className='flex w-full md:w-[60vw] flex-col md:flex-row'>
+                        <div className="w-full md:w-[30%]">Other Profile:</div>
+                        <input type="text" value={otherProfile} onChange={(e) => setOtherProfile(e.target.value)} className='rounded-lg p-1'/>
                     </label>
-                    <label>
-                        Projects:
-                        <input type="text" value={projects} onChange={(e) => setProjects(e.target.value)} />
+                    <label className='flex w-full md:w-[60vw] flex-col md:flex-row'>
+                        <div className="w-full md:w-[30%]">Projects:</div>
+                        <input type="text" value={projects} onChange={(e) => setProjects(e.target.value)} className='rounded-lg p-1'/>
                     </label>
-                    <label>
-                        Bio:
-                        <textarea value={bio} onChange={(e) => setBio(e.target.value)} />
+                    <label className='flex w-full md:w-[60vw] flex-col md:flex-row'>
+                        <div className="w-full md:w-[30%]">Bio:</div>
+                        <textarea value={bio} onChange={(e) => setBio(e.target.value)} className='rounded-lg p-1'/>
                     </label>
-                    <button type="submit">Complete Profile</button>
+                    <button type="submit" className='bg-[#aeaeae] px-4 py-2 rounded-lg mx-auto w-fit'>Complete Profile</button>
                 </form>
             </div>
         </main>
